ad314cfddb937051539b2830157c9e9207cb9418
ahad <ahad.ahmed@surecash.net>
2021-01-17 17:41:46 +0600

    Configure and test log file rolling policy based on size.

--- productservice/src/main/java/com/maico/ms/productservice/controller/rest/ProductRestController.java
+++ productservice/src/main/java/com/maico/ms/productservice/controller/rest/ProductRestController.java
@@ -1,18 +1,33 @@
 package com.maico.ms.productservice.controller.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * @author ahadahmed - 1/12/21
  * @see <a href="#"> see this</a>
  */
-@RestController("/api/")
+@RestController
+@RequestMapping("/api")
 public class ProductRestController {
 
-    @GetMapping("product/{id}")
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @GetMapping("/product/{id}")
     public Product getProduct(@PathVariable Long id){
         return null;
+    }
+
+    @GetMapping("/product/log")
+    public void logTest(){
+
+        this.logger.info("Info looogerereraldfhlajsldfjlasjldfjlasjldfjlasj");
+        this.logger.debug("Debug looogerereraldfhlajsldfjlasjldfjlasjldfjlasj");
+
     }
 }
